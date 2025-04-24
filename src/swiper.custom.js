c4d40396e033bb6e0e6cba352a08039b9dece153
Ihor Hlushko <glushko@zfort.com>
2025-04-24 20:49:23 +0300

    Add rollup

--- src/swiper.custom.js
+++ src/swiper.custom.js
@@ -1,8 +1,8 @@
 /*! Swiper v11 - MIT License - https://swiperjs.com/ */
 
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay, A11y } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 
-Swiper.use([Navigation, Pagination, Autoplay, A11y]);
+Swiper.use([Navigation, Pagination]);
 
 export default Swiper;
